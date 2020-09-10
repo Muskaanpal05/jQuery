989aca9648a497f7d747339b659a2916da6da1b6
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-10 14:49:45 +0530

    Adding Event Listeners using jQuery

--- index.js
+++ index.js
@@ -14,3 +14,22 @@ $("button").html("<strong> Don 't Click Me</strong>")
 
 $("h1").attr("class"); // "big-title" (Getting attribute)
 $("a").attr("href", "https://www.yahoo.com"); //Setting attribute
+
+//Adding Event Listeners using jQuery
+// for (var i = 0; i < document.querySelectorAll("button").length; i++) {
+//     document.querySelectorAll("button")[i].addEventListener("click", function() {
+//         document.querySelector("h1").style.color = "purple";
+//     });
+// }  This is using Javascript
+
+$("button").click(function() {
+    $("h1").css("color", "purple");
+}); //Using jquery
+
+$(document).keypress(function(event) {
+    $("h1").text(event.key);
+});
+
+$("h1").on("mouseover", function() {
+    $("h1").css("color", "red");
+});
