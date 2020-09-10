1dda398a1ddf6d8063ec4a236047570b136ad6d5
Muskaanpal05 <palmuskaan5@gmail.com>
2020-09-10 13:34:09 +0530

    Manipulating text using jQuery

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 $("h1").addClass("big-title margin-50");
 $("h1").removeClass("margin-50");
 //$("h1").hasClass("margin-50") output-->true (Checks for the class contained by element)
+
+//Manipulating text using jQuery
+
+$("h1").text("bye");
+$("h1").html("<em>bye</em>");
+$("button").html("<strong> Don 't Click Me</strong>")
